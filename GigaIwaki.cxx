33af904c500df8419bf4e027ef8f8a52d6e6f413
furuno <maiko.tpc@gmail.com>
2026-04-15 15:15:22 +0900

    stable version during RARiS2026

--- GigaIwaki.cxx
+++ GigaIwaki.cxx
@@ -14,23 +14,20 @@
 #include <syslog.h>
 #include <poll.h>
 #include <unistd.h>
+#include <atomic>
 
 #include "TCPclient.h"
 
 using namespace std;
 
-
-void GigaIwaki(const char *host, bool &end_flag){
+// ★ 签名改成 atomic end_flag + ready
+void GigaIwaki(const char *host, std::atomic<bool> &end_flag, std::atomic<int> &ready){
   string h_tmp = host;
-  int b_id = atoi(&h_tmp[h_tmp.length()-1]);
-  sleep(b_id);
 
-  char ql_id = b_id & 0xff;
-  if(h_tmp.find("anode") != std::string::npos)
-    ql_id += 0x40;
-  else
-    ql_id += 0x80;
-
+  // 你原来这里 sleep(b_id) 是错峰启动，会导致最早几秒必漏数据
+  // 如果你一定要错峰，可以打开下面两行，但那 ready==12 会慢很多。
+  // int b_id = atoi(&h_tmp[h_tmp.length()-1]);
+  // sleep(b_id);
 
   TCPclient iwaki(host, 24);
 
@@ -38,62 +35,80 @@ void GigaIwaki(const char *host, bool &end_flag){
   ifstream ifs_runnum("runnum.dat");
   if(!ifs_runnum){
     cerr << "Cannot read the runnum file." << endl;
-    exit(1);
+    return;
   }
 
   string str_runnum;
   ifs_runnum >> str_runnum;
   int runnum = atoi(str_runnum.c_str());
-  ifs_runnum.close();  
-
+  ifs_runnum.close();
 
   // Read data dir
   ifstream ifs_datadir("./datadir.dat");
   if(!ifs_datadir){
     cerr << "Cannot read the datadir file." << endl;
-    exit(1);
+    return;
   }
 
   string str_datadir;
   ifs_datadir >> str_datadir;
-  ifs_datadir.close();  
-  
+  ifs_datadir.close();
 
   if(iwaki.isAlive()){
-    bool file_opened=0;
+    bool file_opened = false;
     ofstream OutData;
-    unsigned FileNum=0;
+    unsigned FileNum = 0;
 
-    while(!end_flag){
-      if(!file_opened){
-	char filename[300];
-	sprintf(filename, "%s/uTPC_%04d_%s_%05d.raw",
-		str_datadir.c_str(), runnum, host, FileNum);
-	OutData.open(filename);
-	if(OutData)
-	  file_opened = 1;
+    // 先打开文件（binary）
+    {
+      char filename[300];
+      sprintf(filename, "%s/uTPC_%04d_%s_%05d.raw",
+              str_datadir.c_str(), runnum, host, FileNum);
+      OutData.open(filename, ios::out | ios::binary);
+      if(!OutData){
+        cerr << "[" << host << "] Cannot open output file: " << filename << endl;
+        iwaki.Close();
+        return;
       }
-	  
-      char msg[2000];
+      file_opened = true;
+    }
+
+    // ★ 到这里：TCPclient 已创建 + isAlive 成功 + 输出文件已打开
+    //    认为“接收端 ready”，通知主线程
+    int r = ready.fetch_add(1, std::memory_order_relaxed) + 1;
+    // 可选：打印
+    // printf("[%s] ready (%d/12)\n", host, r);
+
+    char msg[2000];
+
+    while(!end_flag.load(std::memory_order_relaxed)){
       int num = iwaki.RecieveMsg(msg, 2000);
-      if(num>0){
-	OutData.write(msg, num);
+      if(num > 0){
+        OutData.write(msg, num);
 
-	if(OutData.tellp() > 0xfffffffff){ // 64GB maximum
-	  OutData.close();
-	  FileNum++;
+        // 64GB rollover：你原来用 tellp 判断OK，但要确保是 binary 且 tellp 可用
+        if(OutData.tellp() > static_cast<std::streampos>(0xfffffffffLL)){
+          OutData.close();
+          FileNum++;
 
-	  char filename[300];
-	  sprintf(filename, "%s/uTPC_%04d_%s_%05d.raw",
-		  str_datadir.c_str(), runnum, host, FileNum);
-	  OutData.open(filename);
-	}
+          char filename[300];
+          sprintf(filename, "%s/uTPC_%04d_%s_%05d.raw",
+                  str_datadir.c_str(), runnum, host, FileNum);
+          OutData.open(filename, ios::out | ios::binary);
+          if(!OutData){
+            cerr << "[" << host << "] Cannot open next file: " << filename << endl;
+            break;
+          }
+        }
+      } else {
+        // num <= 0：可以稍微让一下 CPU，避免空转（视 TCPclient 行为）
+        // usleep(100);
       }
     }
 
-    if(file_opened)
-      OutData.close();
-
+    if(file_opened) OutData.close();
     iwaki.Close();
+  } else {
+    cerr << "[" << host << "] TCPclient is not alive." << endl;
   }
 }
--- MAIKo_DAQ.cxx
+++ MAIKo_DAQ.cxx
@@ -15,17 +15,20 @@
 #include <poll.h>
 #include <signal.h>
 #include <stdlib.h>
+#include <atomic>
 #include <time.h>
 
 #include "vme-gbe-comm.h"
 
 using namespace std;
-void GigaIwaki(const char*, bool&);
 
-bool end_flag;
+static std::atomic<bool> end_flag{false};
+static std::atomic<int>  ready{0};
+
+void GigaIwaki(const char* host, std::atomic<bool>& end_flag, std::atomic<int>& ready);
 
 void EndSeq(int){
-  end_flag = 1;
+  end_flag.store(true, std::memory_order_relaxed);
 }
 
 const std::string VMEGBE2_IP = "10.10.10.36";
@@ -33,22 +36,18 @@ const unsigned short RPV_LEVEL = 0xf00a;
 const unsigned short RPV_PULSE = 0xf008;
 
 int WriteRPV(std::string ip_address,
-	      unsigned short vme_address,
-	      unsigned short data){
+             unsigned short vme_address,
+             unsigned short data){
 
   int ret = 0;
   try{
-    // TCP port 24
     VmeGbeComm comm(ip_address, 24);
-    //Address mode A16
     comm.setAddressMode(VmeGbeComm::A16);
 
     unsigned short buf = htons(data);
 
-    // 2 byte, ID == 1 (arbitrary?)
     ret = comm.write(vme_address, 2, 1,
-		     VmeGbeComm::D16, (char*)&buf);
-    
+                     VmeGbeComm::D16, (char*)&buf);
   }
   catch(std::string &e_str){
     std::cerr << "An exception caught in WriteRPV()" << std::endl;
@@ -57,27 +56,27 @@ int WriteRPV(std::string ip_address,
     std::cerr << std::hex << "data " << data << std::endl;
     std::cerr << e_str;
   }
- catch(std::exception &e_ex){
+  catch(std::exception &e_ex){
     std::cerr << "An exception caught in WriteRPV()" << std::endl;
     std::cerr << "IP " << ip_address << std::endl;
     std::cerr << std::hex << "VME address " << vme_address << std::endl;
     std::cerr << std::hex << "data " << data << std::endl;
     std::cerr << e_ex.what();
   }
- catch(...){
-  std::cerr << "An unexpected exception caught in WriteRPV()" << std::endl;
+  catch(...){
+    std::cerr << "An unexpected exception caught in WriteRPV()" << std::endl;
     std::cerr << "IP " << ip_address << std::endl;
     std::cerr << std::hex << "VME address " << vme_address << std::endl;
     std::cerr << std::hex << "data " << data << std::endl;
- }
+  }
 
   return ret;
-  
 }
 
-
 int main(int argc, char *argv[]){
-  end_flag = 0;
+  end_flag.store(false, std::memory_order_relaxed);
+  ready.store(0, std::memory_order_relaxed);
+
   signal(SIGINT, EndSeq);
 
   int res;
@@ -85,12 +84,11 @@ int main(int argc, char *argv[]){
   if(argc!=2){
     cerr << "" << endl;
     cerr << " USAGE> ./MAIKo_DAQ [run comment]" << endl;
-    cerr << " Example> ./MAIKo_DAQ \"alpha source, upic 400V\" " << endl;    
+    cerr << " Example> ./MAIKo_DAQ \"alpha source, upic 400V\" " << endl;
     cerr << "" << endl;
-    exit(1); 
+    exit(1);
   }
 
-  
   // Read run counter
   ifstream ifs_runnum("runnum.dat");
   if(!ifs_runnum){
@@ -102,14 +100,13 @@ int main(int argc, char *argv[]){
   ifs_runnum >> str_runnum;
   int runnum = atoi(str_runnum.c_str());
   cout << "Run number: " << runnum+1 << endl;
-
   ifs_runnum.close();
 
   // Increment the Run number file
   ofstream ofs_runnum("runnum.dat", ios_base::trunc);
   ofs_runnum << runnum+1 << endl;
   ofs_runnum.close();
-  
+
   // Get start time
   time_t timer;
   struct tm *local;
@@ -119,149 +116,123 @@ int main(int argc, char *argv[]){
   year = local->tm_year + 1900;
   month = local->tm_mon + 1;
   day = local->tm_mday;
-  hour = local->tm_hour;                                                       
-  min = local->tm_min;                                                         
-  sec = local->tm_sec;            
-  
+  hour = local->tm_hour;
+  min = local->tm_min;
+  sec = local->tm_sec;
+
   printf("DAQ start time: %d/%02d/%02d %02d:%02d:%02d\n",
-	 year, month, day, hour, min, sec);
-  
+         year, month, day, hour, min, sec);
+
   // Write Run comment
   string runcom = argv[1];
-  ofstream ofs_runcom("runcom.dat", ios::app);  
+  ofstream ofs_runcom("runcom.dat", ios::app);
   char comment[1024];
   sprintf(comment, "%04d START:%d/%02d/%02d %02d:%02d:%02d  %s",
-	  runnum+1,
-	  year, month, day, hour, min, sec,
-	  runcom.c_str());
+          runnum+1,
+          year, month, day, hour, min, sec,
+          runcom.c_str());
   ofs_runcom << comment << endl;
   ofs_runcom.close();
-  
-  // trigger disable !!!
-  //  sprintf(cmd, "./trg_disable.sh");
-  //  res = system(cmd);
+
+  // trigger disable
   res = WriteRPV(VMEGBE2_IP, RPV_LEVEL, 0x20);
   printf("Trigger disable: %d\n", res);
 
-
-  
-  // visual Scaler reset !!!
-  //  sprintf(cmd, "./sca_reset.sh");
-  //  res = system(cmd);
+  // scaler reset
   res = WriteRPV(VMEGBE2_IP, RPV_PULSE, 0x04);
   printf("Scaler reset: %d\n", res);
 
-  // visual Scaler start !!!
-  //  sprintf(cmd, "./sca_start.sh");
-  //  res = system(cmd);
+  // scaler start
   res = WriteRPV(VMEGBE2_IP, RPV_PULSE, 0x01);
   printf("Scaler start: %d\n", res);
-  
-  
-//  // DAQ disable
-//  sprintf(cmd, "./daq_disable.sh");
-//  res = system(cmd);
-//  printf("DAQ disable: %d\n", res);
 
-  // counter reset !!!
-  //  sprintf(cmd, "./cntrst.sh");
-  //  res = system(cmd);
+  // counter reset
   res = WriteRPV(VMEGBE2_IP, RPV_PULSE, 0x40);
   printf("cntrst: %d\n", res);
 
-  // DAQ enable !!!
-  //  sprintf(cmd, "./daq_enable.sh");
-  //  res = system(cmd);
+  // DAQ enable
   res = WriteRPV(VMEGBE2_IP, RPV_LEVEL, 0xa0);
   printf("DAQ enable: %d\n", res);
 
-  // sleep
-  sleep(2);  
+  sleep(2);
 
-  // Trigger enable !!!
-  //  sprintf(cmd, "./trg_enable.sh");
-  //  res = system(cmd);
+  thread Anode0  (GigaIwaki, "anode0",   ref(end_flag), ref(ready));
+  usleep(50000);
+  thread Anode1  (GigaIwaki, "anode1",   ref(end_flag), ref(ready));
+  usleep(50000);  
+  thread Anode2  (GigaIwaki, "anode2",   ref(end_flag), ref(ready));
+  usleep(50000);  
+  thread Anode3  (GigaIwaki, "anode3",   ref(end_flag), ref(ready));
+  usleep(50000);  
+  thread Anode4  (GigaIwaki, "anode4",   ref(end_flag), ref(ready));
+  usleep(50000);  
+  thread Anode5  (GigaIwaki, "anode5",   ref(end_flag), ref(ready));
+  usleep(50000);  
+  thread Cathode0(GigaIwaki, "cathode0", ref(end_flag), ref(ready));
+  usleep(50000);  
+  thread Cathode1(GigaIwaki, "cathode1", ref(end_flag), ref(ready));
+  usleep(50000);  
+  thread Cathode2(GigaIwaki, "cathode2", ref(end_flag), ref(ready));
+  usleep(50000);  
+  thread Cathode3(GigaIwaki, "cathode3", ref(end_flag), ref(ready));
+  usleep(50000);  
+  thread Cathode4(GigaIwaki, "cathode4", ref(end_flag), ref(ready));
+  usleep(50000);  
+  thread Cathode5(GigaIwaki, "cathode5", ref(end_flag), ref(ready));
+  usleep(50000);  
+  while (ready.load(std::memory_order_relaxed) < 12) usleep(1000);
+  printf("All receiver threads ready: %d\n", ready.load(std::memory_order_relaxed));
+  // reset counter
+ 
+  
   res = WriteRPV(VMEGBE2_IP, RPV_LEVEL, 0x80);
   printf("Trigger enable: %d\n", res);
-  
 
-  printf("Press Cntl-C to stop the DAQ.\n");
+  printf("Press Ctrl-C to stop the DAQ.\n");
 
-  
-  thread Anode0(GigaIwaki, "anode0", ref(end_flag));
-  thread Anode1(GigaIwaki, "anode1", ref(end_flag));
-  thread Anode2(GigaIwaki, "anode2", ref(end_flag));
-  thread Anode3(GigaIwaki, "anode3", ref(end_flag));  
-  thread Anode4(GigaIwaki, "anode4", ref(end_flag));
-  thread Anode5(GigaIwaki, "anode5", ref(end_flag));  
-
-  thread Cathode0(GigaIwaki, "cathode0", ref(end_flag));
-  thread Cathode1(GigaIwaki, "cathode1", ref(end_flag)); 
-  thread Cathode2(GigaIwaki, "cathode2", ref(end_flag));
-  thread Cathode3(GigaIwaki, "cathode3", ref(end_flag)); 
-  thread Cathode4(GigaIwaki, "cathode4", ref(end_flag));
-  thread Cathode5(GigaIwaki, "cathode5", ref(end_flag));  
-
-  while(1){
+  while(true){
     sleep(1);
-    if(end_flag)
-      break;
+    if(end_flag.load(std::memory_order_relaxed)) break;
   }
 
-  Anode0.join();
-  Anode1.join();
-  Anode2.join();
-  Anode3.join(); 
-  Anode4.join();
-  Anode5.join();
-  
-  Cathode0.join();
-  Cathode1.join();  
-  Cathode2.join();
-  Cathode3.join();  
-  Cathode4.join();
-  Cathode5.join();  
+  // Join
+  Anode0.join();  Anode1.join();  Anode2.join();  Anode3.join();  Anode4.join();  Anode5.join();
+  Cathode0.join(); Cathode1.join(); Cathode2.join(); Cathode3.join(); Cathode4.join(); Cathode5.join();
 
   sleep(3);
 
-  // DAQ disable !!!
-  //  sprintf(cmd, "./daq_disable.sh");
-  //  res = system(cmd);
+  // DAQ disable
   res = WriteRPV(VMEGBE2_IP, RPV_LEVEL, 0x00);
   printf("DAQ disable: %d\n", res);
 
-  // trigger disable !!!
-  //  sprintf(cmd, "./trg_disable.sh");
-  //  res = system(cmd);
+  // trigger disable
   res = WriteRPV(VMEGBE2_IP, RPV_LEVEL, 0x20);
   printf("Trigger disable: %d\n", res);
 
-
-  // Scaler stop !!!
-  //  sprintf(cmd, "./sca_stop.sh");
-  //  res = system(cmd);
+  // scaler stop
   res = WriteRPV(VMEGBE2_IP, RPV_PULSE, 0x02);
   printf("Scaler stop: %d\n", res);
 
-  // Get stop time
-  timer = time(NULL);                                                            
-  local = localtime(&timer);                                                     
-  year = local->tm_year + 1900;                                                  
-  month = local->tm_mon + 1;                                                     
+  // stop time
+  timer = time(NULL);
+  local = localtime(&timer);
+  year = local->tm_year + 1900;
+  month = local->tm_mon + 1;
   day = local->tm_mday;
-  hour = local->tm_hour;                                                       
-  min = local->tm_min;                                                         
-  sec = local->tm_sec;            
-  
+  hour = local->tm_hour;
+  min = local->tm_min;
+  sec = local->tm_sec;
+
   printf("DAQ stop time: %d/%02d/%02d %02d:%02d:%02d\n",
-	 year, month, day, hour, min, sec);
-  
-  // write run comment
-  ofstream ofs_runcom2("runcom.dat", ios::app);  
+         year, month, day, hour, min, sec);
+
+  // write run comment stop
+  ofstream ofs_runcom2("runcom.dat", ios::app);
   sprintf(comment, "STOP:%d/%02d/%02d %02d:%02d:%02d\n",
-	  year, month, day, hour, min, sec);
+          year, month, day, hour, min, sec);
   ofs_runcom2 << comment << endl;
   ofs_runcom2.close();
 
   signal(SIGINT, SIG_DFL);
+  return 0;
 }
